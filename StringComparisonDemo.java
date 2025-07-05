441dc96bff2a8ee0a8d6e9eef8e891c06e312e02
Oweta Jacob Emmy <oweta001@gmail.com>
2025-07-05 13:33:59 +0300

    files update

--- StringComparisonDemo.java
+++ StringComparisonDemo.java
@@ -1,3 +1,10 @@
 public class StringComparisonDemo {
-    
+    public static void main(String[] args) {
+        String author1 = "Ainebyoona";
+        String author2 = "ainebyoona";
+        String author3 = new String("Ainebyoona");
+
+        System.out.println(author1 == author3); // false
+        System.out.println(author1.equalsIgnoreCase(author2)); // true
+    }
 }
--- StringSearchFunctions.java
+++ StringSearchFunctions.java
@@ -1,3 +1,16 @@
 public class StringSearchFunctions {
-    
+    public static void main(String[] args) {
+        String userQuery = "animal farm";
+        String bookTitle = "Animal Farm";
+
+        // Using equalsIgnoreCase
+        if (userQuery.equalsIgnoreCase(bookTitle)) {
+            System.out.println("Match found using equalsIgnoreCase");
+        }
+
+        // Using toLowerCase
+        if (userQuery.toLowerCase().equals(bookTitle.toLowerCase())) {
+            System.out.println("Match found using toLowerCase");
+        }
+    }
 }
